30155faf6d1a1fef49f0ab0060573fa991118b68
eLee-star-one <lixingxing@Longshine.com>
2020-12-23 17:05:52 +0800

    更新结构

--- docs/.vuepress/components/button-demos.vue
+++ docs/.vuepress/components/button-demos.vue
@@ -1,23 +1,28 @@
 <template>
   <div>
-    <g-button>默认按钮</g-button>
-    <g-button icon="settings">默认按钮</g-button>
-    <g-button :loading="true">默认按钮</g-button>
-    <g-button disabled>默认按钮</g-button>
-
+    <g-button icon="setting">设置</g-button>
+            <g-button icon="setting" :loading="loading1" @click="loading1=!loading1">设置</g-button>
+            <g-button>设置</g-button>
+            <g-button icon="setting" icon-position="right">设置</g-button>
+            <g-button icon="setting" icon-position="right" :loading="loading2" @click="loading2=!loading2">设置</g-button>
     <pre>
       <code>{{content}}</code>
     </pre>
   </div>
 </template>
 <script>
-import Button from '../../../src/button/button'
+import Vue from "vue"
+import "test_gulu_lee/dist/gulu.css"
+import {Button} from "test_gulu_lee"
 export default {
+  name:"button-demos",
   components:{
-    'g-button':Button
+    "g-button":Button
   },
   data(){
     return{
+      loading1: true,
+        loading2: false,
       content:`
         <g-button>默认按钮</g-button>
         <g-button icon="settings">默认按钮</g-button>
--- docs/components/button.md
+++ docs/components/button.md
@@ -5,6 +5,6 @@ sidebarDepth: 2
 # 按钮
 使用方法
 <ClientOnly>
-<button-demos></button-demos>
+<button-demos>555</button-demos>
 </ClientOnly>
  
